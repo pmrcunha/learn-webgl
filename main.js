b11f7ce38a5f64a5fc40392972fc1f2087523be9
Paulo Cunha <pc@paulocunha.me>
2018-01-26 13:52:35 +0100

    annotate the code

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// vertex shader and fragment shader written GLSL
+// the vertex shader is executed for each vertex of the shape
+
+/*
+In WebGL, values that apply to a specific vertex are stored in attributes. These are only available to the JavaScript code and the vertex shader. Attributes are referenced by an index number into the list of attributes maintained by the GPU
+*/
+
 const vsSource = `
   precision mediump float;
 
@@ -10,7 +17,7 @@ const vsSource = `
     gl_Position = vec4(vertPosition, 0.0, 1.0);
   }
 `;
-
+// the fragment shader is executed for each pixel to be rendered
 const fsSource = `
   precision mediump float;
 
@@ -21,6 +28,7 @@ const fsSource = `
   }
 `;
 
+// loadShader takes the source code for a shader, compiles it, and logs errors
 const loadShader = (gl, type, source) => {
   const shader = gl.createShader(type);
 
@@ -34,6 +42,7 @@ const loadShader = (gl, type, source) => {
   return shader;
 }
 
+// link the compiled shaders into a webGL program
 const initShaderProgram = (gl, vsSource, fsSource) => {
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
@@ -48,6 +57,7 @@ const initShaderProgram = (gl, vsSource, fsSource) => {
     return null;
   }
 
+  // NOTE this validates doesnt work for some reason
   // if (!gl.getProgramParameter(shaderProgram, gl.VALIDATE_STATUS)) {
   //   console.error('Error validating shader program!', gl.getProgramInfoLog(shaderProgram));
   //   return null;
@@ -56,6 +66,7 @@ const initShaderProgram = (gl, vsSource, fsSource) => {
   return shaderProgram;
 }
 
+// initializes the buffer with the points and other data that the shader program will execute upon
 const initBuffers = (gl, shaderProgram) => {
   const triangleVertices = [
     // X, Y,    R,G,B,
@@ -65,8 +76,15 @@ const initBuffers = (gl, shaderProgram) => {
   ];
 
   const triangleVertexBufferObject = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject); // gl.ARRAY_BUFFER is a binding point, to which we are binding our webGL buffer object
+
+  // Javascript's number type is always a 64 bit float (or something like that)
+  // Float32Array casts the numbers in the array to 32 bit floats
+  // initializes the buffer at the target/binding point and stores data in it
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+  // static draw means that the contents of the buffer are likely to be used often, but rarely changed
+  // gl.DYNAMIC_DRAW should be used for buffers that are used often and change often
+  // gl.STREAM_DRAW should be used for buffers that are rarely used but change often
 
   // attribute locations in memory
   const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'vertPosition');
@@ -74,22 +92,24 @@ const initBuffers = (gl, shaderProgram) => {
   {
     const elementsPerAttribute = 2;
     const typeOfElements = gl.FLOAT;
-    const sizeOfIndividualVertex = 5 * Float32Array.BYTES_PER_ELEMENT;
+    const sizeOfIndividualVertex = 5 * Float32Array.BYTES_PER_ELEMENT; // 5 numbers times the memory size of each number
     const offsetFromVertexStartToThisAttribute = 0;
+    // specifies the memory layout of the buffer
     gl.vertexAttribPointer(
       positionAttribLocation,
       elementsPerAttribute,
       typeOfElements,
-      gl.FALSE,
+      gl.FALSE, // gl boolean specifying whether integer data values should be normalized into a certain range when being casted to a float
       sizeOfIndividualVertex,
       offsetFromVertexStartToThisAttribute
     );
   }
+  // block scoping is awesome
   {
     const elementsPerAttribute = 3;
     const typeOfElements = gl.FLOAT;
     const sizeOfIndividualVertex = 5 * Float32Array.BYTES_PER_ELEMENT;
-    const offsetFromVertexStartToThisAttribute = 2 * Float32Array.BYTES_PER_ELEMENT;
+    const offsetFromVertexStartToThisAttribute = 2 * Float32Array.BYTES_PER_ELEMENT; // skip the position values to get to the color values
     gl.vertexAttribPointer(
       colorAttribLocation,
       elementsPerAttribute,
@@ -100,6 +120,9 @@ const initBuffers = (gl, shaderProgram) => {
     );
   }
 
+  /*
+  Attributes must be enabled before they can be used by other methods
+  */
   gl.enableVertexAttribArray(positionAttribLocation);
   gl.enableVertexAttribArray(colorAttribLocation);
 }
@@ -113,14 +136,24 @@ const init = () => {
     return;
   }
 
-  gl.clearColor(0.75, 0.85, 0.8, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  gl.clearColor(0.75, 0.85, 0.8, 1.0); // sets the color that the screen will be cleared to
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // clears the screen at all different depths (shapes render over previously rendered shapes)
 
   const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-  initBuffers(gl, shaderProgram);
+  initBuffers(gl, shaderProgram); // needs the shader program to get a pointer to the attributes
 
-  gl.useProgram(shaderProgram);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.useProgram(shaderProgram); // sets a webGl / shader program to be used
+  /*
+  render primitives from array data
+  gl.POINTS draws a single dot
+  gl.LINES draws lines between 2 vertex
+  gl.TRIANGLES draws a face/triangle from 3 vertex
+  */
+  gl.drawArrays(
+    gl.TRIANGLES, // tyoe primitive to render. see above
+    0, // starting index in the array of vertex points
+    3 // number of indices to be rendered
+  );
 }
 
 init();
