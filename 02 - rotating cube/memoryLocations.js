2d286aa8ef4efd19a68455609a423a3cf6fd129b
Paulo Cunha <paulomrcunha@gmail.com>
2019-04-15 22:03:22 +0100

    Add a pattern as a fragment shader

--- 02 - rotating cube/memoryLocations.js
+++ 02 - rotating cube/memoryLocations.js
@@ -1,13 +1,19 @@
-import gl from './setupCanvas';
-import shaderProgram from './shaderProgram';
+import gl from "./setupCanvas";
+import shaderProgram from "./shaderProgram";
 
 // Locations in GPU memory
-const mWorld = gl.getUniformLocation(shaderProgram, 'mWorld');
-const mView = gl.getUniformLocation(shaderProgram, 'mView');
-const mProj = gl.getUniformLocation(shaderProgram, 'mProj');
+const mWorld = gl.getUniformLocation(shaderProgram, "mWorld");
+const mView = gl.getUniformLocation(shaderProgram, "mView");
+const mProj = gl.getUniformLocation(shaderProgram, "mProj");
+
+// These are the uniforms of the more complex fragment shader fsSource2
+const u_resolution = gl.getUniformLocation(shaderProgram, "u_resolution");
+const u_time = gl.getUniformLocation(shaderProgram, "u_time");
 
 export default {
   mWorld,
   mView,
   mProj,
-}
+  u_resolution,
+  u_time
+};
